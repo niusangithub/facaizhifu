809eb93281725e98e9569e6c8323fdf655a1bfb1
lanleiming <lanleiming@360.cn>
2019-03-26 00:08:01 +0800

    111

--- 头条多多.js
+++ 头条多多.js
@@ -2,30 +2,43 @@
 var Common = {
     width: device.width, //设备的宽
     height: device.height, //设备的高
-    startAPP: function (appName) {
-        var isHasApp = launchApp(appName);
+    //通过名字||包名，启动app
+    startAPP: function (name) {
+        var isHasApp = launchApp(name);
         if (!isHasApp) {
-            toast('手机未安装：' + appName);
+            toast('手机未安装：' + name);
         } else {
+            //设置屏幕缩放
+            setScreenMetrics(1080, 1920);
             sleep(5000); //停顿5s
-            var dom_allow = text('允许').find();
-            if (!dom_allow.empty()) {
-                toast('找到了');
+            var dom_allow = this.findDomByText('允许');
+            if (dom_allow) {               
                 dom_allow.click();
-            } else {
-                toast('没找到');
-            }
+            } 
         }
         return isHasApp;
     },
-    closeApp:function(){
-        for(var i=0;i<4;i++){
-            back();
+    //通过包名，关闭app
+    closeApp:function(packageName){
+        try{
+            packageName =packageName || currentPackage();
+            var result = app.openAppSetting(packageName);
+            if(result){
+                sleep(2000);
+                this.findDomByText('结束运行').click();
+                sleep(1000);
+                this.findDomByText('确定').click();
+                home();
+                sleep(200);
+                home();
+            }
+        }catch(ex){
+            home();
             sleep(200);
+            home();
         }
-        sleep(1000);
-        click(120,1300);
     },
+    //通过id，查找dom
     findDomById: function (idStr) {
         var dom_txt = id(idStr).find();
         if (dom_txt.empty()) {
@@ -36,6 +49,7 @@ var Common = {
             return dom_txt;
         }
     },
+    //通过文本，查找dom
     findDomByText: function (txt) {
         var dom_txt = text(txt).find();
         if (dom_txt.empty()) {
@@ -46,6 +60,18 @@ var Common = {
             return dom_txt;
         }
     },
+    //通过描述desc，查找dom
+    findDomByDesc:function(txt){
+        var dom_txt = desc(txt).find();
+        if (dom_txt.empty()) {
+            toast('没有找到：' + txt);
+            return null;
+        } else {
+            toast('找到了：' + txt);
+            return dom_txt;
+        }
+    },
+    //在某个范围内通过text寻找控件
     findDomInsideByText:function(txt,x,y,x1,y1){
         var dom_txt = text(txt).boundsInside(x,y,x1,y1).find();
         if (dom_txt.empty()) {
@@ -55,12 +81,58 @@ var Common = {
             toast('找到了：' + txt);
             return dom_txt;
         }
+    },
+    //通过id，点击控件
+    clickById:function(idStr){
+        var isflag = false; //是否存在
+        var dom = id(idStr).findOnce();
+        if(dom){
+            isflag = true;
+            dom.click();
+        }
+        sleep(1000);
+        return isflag;
+    },
+    //通过text，点击控件
+    clickByText:function(txt){
+        var isflag = false; //是否存在
+        var dom = text(txt).findOnce();
+        if(dom){
+            isflag = true;
+            dom.click();
+        }
+        sleep(1000);
+        return isflag;
+    },
+     //通过desc，点击控件
+    clickByDesc:function(txt){
+        var isflag = false; //是否存在
+        var dom = desc(txt).findOnce();
+        if(dom){
+            isflag = true;
+            dom.click();
+        }
+        sleep(1000);
+        return isflag;
+    },
+    //以下方法，root权限才可用
+    tap: function (x, y) {
+        var ra = new RootAutomator();
+        //让"手指1"点击位置(100, 100)
+        toast("点击");
+        ra.press(x, y, 1);
+        ra.exit();
+    },
+    swipe: function () {
+        var ra = new RootAutomator();
+        ra.swipe(Common.width / 2, Common.height * 0.8, Common.width / 2, Common.height / 8 * 0.4, 1000);
+        ra.exit();
     }
+    
 };
-
 //头条多多
 var toutiaoduoduo = {
-    appName: '头条多多',
+    appName: '头条多多', 
     init: function () {
         toast(this.appName);
         var isHasApp = Common.startAPP(this.appName);
@@ -151,5 +223,6 @@ var toutiaoduoduo = {
         // console.log(dom_adv);
         // console.show();
     }
-
 }
+
+toutiaoduoduo.init();
