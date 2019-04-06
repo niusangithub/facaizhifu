7819f232d2bdd6364cf13c2302599415f92720eb
lanleiming <lanleiming@126.com>
2019-04-06 16:25:42 +0800

    demo

--- 刷宝视频.js
+++ 刷宝视频.js
@@ -1,143 +1,13 @@
 "auto";
-var Common = {
-    width: device.width, //设备的宽
-    height: device.height, //设备的高
-    //通过名字||包名，启动app
-    startAPP: function (name) {
-        var isHasApp = launchApp(name);
-        if (!isHasApp) {
-            toast('手机未安装：' + name);
-        } else {
-            //设置屏幕缩放
-            setScreenMetrics(1080, 1920);
-            sleep(5000); //停顿5s
-            var dom_allow = this.findDomByText('允许');
-            if (dom_allow) {               
-                dom_allow.click();
-            } 
-        }
-        return isHasApp;
-    },
-    //通过包名，关闭app
-    closeApp:function(packageName){
-        try{
-            packageName =packageName || currentPackage();
-            var result = app.openAppSetting(packageName);
-            if(result){
-                toast('找到了app');
-                sleep(2000);
-                this.findDomByText('结束运行').click();
-                sleep(1000);
-                this.findDomByText('确定').click();
-                home();
-                sleep(200);
-                home();
-            }
-        }catch(ex){
-            toast('没有找到app');
-            home();
-            sleep(200);
-            home();
-        }
-    },
-    //通过id，查找dom
-    findDomById: function (idStr) {
-        var dom_txt = id(idStr).find();
-        if (dom_txt.empty()) {
-            toast('没有找到：' + idStr);
-            return null;
-        } else {
-            toast('找到了：' + idStr);
-            return dom_txt;
-        }
-    },
-    //通过文本，查找dom
-    findDomByText: function (txt) {
-        var dom_txt = text(txt).find();
-        if (dom_txt.empty()) {
-            toast('没有找到：' + txt);
-            return null;
-        } else {
-            toast('找到了：' + txt);
-            return dom_txt;
-        }
-    },
-    //通过描述desc，查找dom
-    findDomByDesc:function(txt){
-        var dom_txt = desc(txt).find();
-        if (dom_txt.empty()) {
-            toast('没有找到：' + txt);
-            return null;
-        } else {
-            toast('找到了：' + txt);
-            return dom_txt;
-        }
-    },
-    //在某个范围内通过text寻找控件
-    findDomInsideByText:function(txt,x,y,x1,y1){
-        var dom_txt = text(txt).boundsInside(x,y,x1,y1).find();
-        if (dom_txt.empty()) {
-            toast('没有找到：' + txt);
-            return null;
-        } else {
-            toast('找到了：' + txt);
-            return dom_txt;
-        }
-    },
-    //通过id，点击控件
-    clickById:function(idStr){
-        var isflag = false; //是否存在
-        var dom = id(idStr).findOnce();
-        if(dom){
-            isflag = true;
-            dom.click();
-        }
-        sleep(1000);
-        return isflag;
-    },
-    //通过text，点击控件
-    clickByText:function(txt){
-        var isflag = false; //是否存在
-        var dom = text(txt).findOnce();
-        if(dom){
-            isflag = true;
-            dom.click();
-        }
-        sleep(1000);
-        return isflag;
-    },
-     //通过desc，点击控件
-    clickByDesc:function(txt){
-        var isflag = false; //是否存在
-        var dom = desc(txt).findOnce();
-        if(dom){
-            isflag = true;
-            dom.click();
-        }
-        sleep(1000);
-        return isflag;
-    },
-    //以下方法，root权限才可用
-    tap: function (x, y) {
-        var ra = new RootAutomator();
-        //让"手指1"点击位置(100, 100)
-        toast("点击");
-        ra.press(x, y, 1);
-        ra.exit();
-    },
-    swipe: function () {
-        var ra = new RootAutomator();
-        ra.swipe(Common.width / 2, Common.height * 0.8, Common.width / 2, Common.height / 8 * 0.4, 1000);
-        ra.exit();
-    }
-    
-};
+const utils = require('./utils');
+const autoUtils = utils.init(true);
+
 //刷宝app
 var shuabao = {
     packageName:'刷宝',
     init:function(){
         toast('启动刷宝app');
-        var isHasApp = Common.startAPP(this.packageName);
+        var isHasApp = autoUtils.startAPP(this.packageName);
         if(!isHasApp)return;     
         sleep(15000);//等待15s
         this.closeTongZhi();
@@ -146,14 +16,14 @@ var shuabao = {
     },
     //关闭通知权限
     closeTongZhi:function(){
-        var dom_cancle = Common.findDomByText('取消');
+        var dom_cancle = autoUtils.findDomByText('取消');
         if(dom_cancle){
             dom_cancle.click();
             sleep(1000);
         }
     },
     closeTaskBox:function(){
-        var dom_close = Common.findDomById('imgClose');
+        var dom_close = autoUtils.findDomById('imgClose');
         if(dom_close){
             dom_close.click();
         }
@@ -168,7 +38,7 @@ var shuabao = {
         this.closeTaskBox();
         sleep(3300);
         //签到
-        var dom_sign = Common.findDomByText('立即签到');
+        var dom_sign = autoUtils.findDomByText('立即签到');
         if(dom_sign){
             dom_sign.click();
             sleep(500); 
@@ -188,7 +58,7 @@ var shuabao = {
             }
             sleep(sleepCount);
             click(dom_task_bounds.centerX(),dom_task_bounds.centerY());
-           // swipe(Common.width/2,Common.height/8*7,Common.width/2,Common.height/8*1,1000);
+           // swipe(autoUtils.width/2,autoUtils.height/8*7,autoUtils.width/2,autoUtils.height/8*1,1000);
         }
     }
 };
